5799095056ec5c2232d055b5058a47c2e4dc4a6b
mepto <natg.inf@gmail.com>
2022-09-04 21:11:02 +0200

    Update header and urls

--- litreview/urls.py
+++ litreview/urls.py
@@ -4,9 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from litreview.views.main import Homepage, SignUp, UserProfile
-from litreview.views.review import ReviewCreateView, ReviewListView
+from litreview.views.review import ReviewCreateView, ReviewDeleteView, ReviewEditView, ReviewListView
 from litreview.views.ticket import TicketCreateView, TicketDeleteView, TicketEditView, TicketListView
-
 
 urlpatterns = [
     path('home/', Homepage.as_view(), name='home'),
@@ -17,9 +16,12 @@ urlpatterns = [
     path('ticket/edit/<int:pk>/', TicketEditView.as_view(), name='ticket-edit'),
     path('ticket/delete/<int:pk>/', TicketDeleteView.as_view(), name='ticket-delete'),
     path('ticket/list/', TicketListView.as_view(), name='tickets'),
+    path('review/new/<int:ticket>/', ReviewCreateView.as_view(), name='review-new'),
     path('review/new/', ReviewCreateView.as_view(), name='review-new'),
+    path('review/edit/<int:pk>/', ReviewEditView.as_view(), name='review-edit'),
+    path('review/delete/<int:pk>/', ReviewDeleteView.as_view(), name='review-delete'),
     path('review/list/', ReviewListView.as_view(), name='reviews'),
 ]
 
 if settings.DEBUG:
-        urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
