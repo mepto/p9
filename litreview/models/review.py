30b3994230a9894f914a1331b187f374a119de48
mepto <natg.inf@gmail.com>
2022-09-11 16:40:56 +0200

    Update review model

--- litreview/models/review.py
+++ litreview/models/review.py
@@ -12,12 +12,14 @@ class Review(models.Model):
     """Store the users' reviews."""
     ticket = models.ForeignKey(to=Ticket, on_delete=models.CASCADE)
     rating = models.PositiveSmallIntegerField(validators=[
-        MinValueValidator(0), MaxValueValidator(5)
-    ])
+        MinValueValidator(0), MaxValueValidator(5)], verbose_name='Book rating')
     user = models.ForeignKey(to=User, on_delete=models.CASCADE)
-    headline = models.CharField(max_length=128)
-    body = models.TextField(max_length=8192, blank=True)
+    headline = models.CharField(max_length=128, verbose_name='Review headline')
+    body = models.TextField(max_length=8192, verbose_name='Review')
     time_created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-time_created']
 
     def __str__(self):
         return self.headline
